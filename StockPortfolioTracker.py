7fa2d69bc72365140dfe5aa4a806bd9f681cd519
nikhxxt <nikhxtwork@gmail.com>
2024-07-30 14:34:35 +0530

    Update StockPortfolioTracker.py

--- StockPortfolioTracker.py
+++ StockPortfolioTracker.py
@@ -1,1 +1,10 @@
-# CodeAlpha_Project_StockNEW
+import requests
+from datetime import datetime
+
+
+API_KEY = 'your_alpha_vantage_api_key'
+
+class Portfolio:
+    def __init__(self):
+      self.holdings = {}
+      
